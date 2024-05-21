7e1390a4873e9f2501aec2aa26940abfa6c0983a
krzasa <krzasa96@gmail.com>
2024-05-21 15:54:46 -0400

    Added logic to app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import { useState, useEffect } from 'react';
 import * as jukeboxService from './services/jukeboxService';
 const App = () => {
-  return <h1>Hello world!</h1>;
+const [trackList, setTrackList] = useState([])
+
+
+useEffect(() => {
+  const fetchTracks = async () => {
+    try {
+    const tracks = await jukeboxService.index();
+    if (tracks.error) {
+      throw new Error(tracks.error);
+    }
+    setTrackList(tracks);
+  } catch (error) {
+    console.log(error);
+  }
+  };
+  fetchTracks();
+
+
+}, []);
+  // return <TrackList tracklist={trackList} />
 };
 
 export default App;
