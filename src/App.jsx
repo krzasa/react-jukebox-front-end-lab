30fb60aebb097cfc1b81125aec37a5af818ef4a9
Yelnats <s.jeanjr@gmail.com>
2024-05-21 15:45:44 -0400

    added form logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import { useState, useEffect } from 'react';
 import * as jukeboxService from './services/jukeboxService';
 const App = () => {
+
+
+
+
+
+
+
+
+
+  
   return <h1>Hello world!</h1>;
 };
 
