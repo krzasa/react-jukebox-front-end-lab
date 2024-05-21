6ce3cfa73bc0af6e1d700e6cef3a40c9b32af3ca
Yelnats <s.jeanjr@gmail.com>
2024-05-21 17:18:32 -0400

    created if else for Tracklist and imported to App.jsx

--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,12 +1,10 @@
 const trackList = (props) => {
     const tracks = props.trackList.map((track) => <li key={track._id}>{track.name}</li>);
 
-
-
     return (
         <div>
         <h1>Track List</h1>
-        <ul>{tracks}</ul>
+        {!props.trackList.length ? <h2>No queued tracks!</h2> : <ul>{tracks}</ul>}
         </div>
   );
 };
