3084c2b9735e0daf32c5a46e6520675ead80430d
krzasa <krzasa96@gmail.com>
2024-05-21 15:03:08 -0400

    Added jukeboxService

--- src/services/jukeboxService.js
+++ src/services/jukeboxService.js
@@ -1,1 +1,14 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+const show = async (track) => {
+    try {
+    const res = await fetch(BASE_URL + `/${track}`)
+    const data = await res.json()
+   console.log(data)
+   return data
+   
+    } catch (err) {
+        console.log(err);
+    }
+  };
+
+export { show } ;
