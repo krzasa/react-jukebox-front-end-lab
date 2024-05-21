819adf47d5294d819dbfc1de841b304cdc140f25
Yelnats <s.jeanjr@gmail.com>
2024-05-21 16:16:48 -0400

    readjusted useEffect variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import { useState, useEffect } from 'react';
 import * as jukeboxService from './services/jukeboxService';
 const App = () => {
+  const [trackList, setTrackList] = useState([])
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const tracks = await jukeboxService.index();
+        if (tracks.error) {
+          throw new Error(tracks.error);
+        }
+        setTrackList(tracks);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTracks();
+  }, [])
 
 
 
@@ -9,32 +25,11 @@ const App = () => {
 
 
 
-
-
-  
-  return <h1>Hello world!</h1>;
-=======
-const [trackList, setTrackList] = useState([])
-
-
-useEffect(() => {
-  const fetchTracks = async () => {
-    try {
-    const tracks = await jukeboxService.index();
-    if (tracks.error) {
-      throw new Error(tracks.error);
-    }
-    setTrackList(tracks);
-  } catch (error) {
-    console.log(error);
-  }
-  };
-  fetchTracks();
-
-
-}, []);
+  return (
+  <h1>Hello world!</h1>
+  );
   // return <TrackList tracklist={trackList} />
-
+  
 };
 
 export default App;
